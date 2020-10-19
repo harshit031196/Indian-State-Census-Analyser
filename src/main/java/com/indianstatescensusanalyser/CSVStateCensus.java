8c26e495f6f3cef52fbb5d74105bb526a06870f7
harshit031196 <guptaharshit0311@gmail.com>
2020-10-20 00:21:11 +0530

    Added the refactoring to follow DRY principle

--- src/main/java/com/indianstatescensusanalyser/CSVStateCensus.java
+++ src/main/java/com/indianstatescensusanalyser/CSVStateCensus.java
@@ -17,28 +17,66 @@ public class CSVStateCensus {
 
 	public int loadStateCensusData(String filePath) throws AnalyserException {
 		try (Reader reader = Files.newBufferedReader(Paths.get(filePath));) {
-			if (!(filePath.split("\\.")[1].equals("csv"))) {
-				throw new AnalyserException(AnalyserExceptionType.INCORRECT_TYPE, "Incorrect File Type");
-			}
-			BufferedReader bufferedReader = Files.newBufferedReader(Paths.get(filePath));
-			String[] headerColumns = bufferedReader.readLine().split(",");
-			if (headerColumns.length < 5) {
-				throw new AnalyserException(AnalyserExceptionType.INCORRECT_DELIMITER, "File contains Invalid Delimiter");
-			}
-			if (!headerColumns[1].equals("State Name") || !headerColumns[2].equals("TIN")
-					|| !headerColumns[3].equals("Population") || !headerColumns[4].equals("State Code")) {
-				throw new AnalyserException(AnalyserExceptionType.INCORRECT_HEADER, "Incorrect Header");
-			}
-			bufferedReader.close();
-			CsvToBean<IndiaStateCensusData> csvToBean = new CsvToBeanBuilder<IndiaStateCensusData>(reader)
-					                                          .withType(IndiaStateCensusData.class).withIgnoreLeadingWhiteSpace(true).build();
-			Iterator<IndiaStateCensusData> iterator = csvToBean.iterator();
+			Iterator<IndiaStateCensusData> iterator = getCSVFileIterator(reader, IndiaStateCensusData.class, filePath);
 			Iterable<IndiaStateCensusData> csvIterable = () -> iterator;
 			return (int) StreamSupport.stream(csvIterable.spliterator(), false).count();
-		} catch (IllegalStateException e) {
-			throw new AnalyserException(AnalyserExceptionType.UNABLE_TO_PARSE, "Unable to parse");
 		} catch (IOException e) {
 			throw new AnalyserException(AnalyserExceptionType.FILE_PROBLEM, "File problem encountered");
 		}
+	}
+
+	public int loadStateCodeData(String filePath) throws AnalyserException {
+		try (Reader reader = Files.newBufferedReader(Paths.get(filePath));) {
+			Iterator<IndianStateCode> iterator = getCSVFileIterator(reader, IndianStateCode.class, filePath);
+			Iterable<IndianStateCode> csvIterable = () -> iterator;
+			return (int) StreamSupport.stream(csvIterable.spliterator(), false).count();
+		} catch (IOException e) {
+			throw new AnalyserException(AnalyserExceptionType.FILE_PROBLEM, "File problem encountered");
+		}
+	}
+
+	private <E> Iterator<E> getCSVFileIterator(Reader reader, Class<E> csvClass, String filePath)
+			throws AnalyserException {
+		try {
+			if (!((filePath.split("\\.")[1]).equals("csv"))) {
+				throw new AnalyserException(AnalyserExceptionType.INCORRECT_TYPE, "Incorrect file type");
+			}
+			if (!isCorrectDelimiter(filePath)) {
+				throw new AnalyserException(AnalyserExceptionType.INCORRECT_DELIMITER, "File contains Invalid Delimiter");
+			}
+			if (!isCorrectHeader(filePath)) {
+				throw new AnalyserException(AnalyserExceptionType.INCORRECT_HEADER, "Incorrect Header");
+			}
+			CsvToBean<E> csvToBean = new CsvToBeanBuilder<E>(reader).withType(csvClass)
+					                                                .withIgnoreLeadingWhiteSpace(true)
+					                                                .build();
+			return csvToBean.iterator();
+		} catch (IllegalStateException e) {
+			throw new AnalyserException(AnalyserExceptionType.UNABLE_TO_PARSE, "Unable to parse");
+		}
+	}
+
+	public boolean isCorrectHeader(String filePath) throws AnalyserException {
+		try (BufferedReader bufferedReader = Files.newBufferedReader(Paths.get(filePath));) {
+			String[] headerColumns = bufferedReader.readLine().split(",");
+			if (!headerColumns[1].equals("State Name") || !headerColumns[2].equals("TIN")
+					|| !headerColumns[3].equals("Population") || !headerColumns[4].equals("State Code"))
+				return false;
+		} catch (IOException e) {
+			throw new AnalyserException(AnalyserExceptionType.FILE_PROBLEM, "File Problem Occured");
+		}
+		return true;
+	}
+
+	public boolean isCorrectDelimiter(String filePath) throws AnalyserException {
+		try (BufferedReader bufferedReader = Files.newBufferedReader(Paths.get(filePath));) {
+			String[] headerColumns = bufferedReader.readLine().split(",");
+			if (headerColumns.length < 5) {
+				return false;
+			}
+		} catch (IOException e) {
+			throw new AnalyserException(AnalyserExceptionType.FILE_PROBLEM, "File Problem Occured");
+		}
+		return true;
 	}
 }
