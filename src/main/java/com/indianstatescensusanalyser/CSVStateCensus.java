ec5546ed0b79bb769317aa9d35023e5fa281c698
harshit031196 <guptaharshit0311@gmail.com>
2020-10-20 00:35:37 +0530

    Added the refactoring to handle single responsibility principle

--- src/main/java/com/indianstatescensusanalyser/CSVStateCensus.java
+++ src/main/java/com/indianstatescensusanalyser/CSVStateCensus.java
@@ -1,6 +1,5 @@
 package com.indianstatescensusanalyser;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
@@ -8,16 +7,13 @@ import java.nio.file.Paths;
 import java.util.Iterator;
 import java.util.stream.StreamSupport;
 
-import com.opencsv.bean.CsvToBean;
-import com.opencsv.bean.CsvToBeanBuilder;
-
 import com.indianstatescensusanalyser.AnalyserException.AnalyserExceptionType;
 
 public class CSVStateCensus {
 
 	public int loadStateCensusData(String filePath) throws AnalyserException {
 		try (Reader reader = Files.newBufferedReader(Paths.get(filePath));) {
-			Iterator<IndiaStateCensusData> iterator = getCSVFileIterator(reader, IndiaStateCensusData.class, filePath);
+			Iterator<IndiaStateCensusData> iterator = new OpenCSVBuilder().getCSVFileIterator(reader, IndiaStateCensusData.class, filePath);
 			return getCount(iterator);
 		} catch (IOException e) {
 			throw new AnalyserException(AnalyserExceptionType.FILE_PROBLEM, "File problem encountered");
@@ -26,57 +22,14 @@ public class CSVStateCensus {
 
 	public int loadStateCodeData(String filePath) throws AnalyserException {
 		try (Reader reader = Files.newBufferedReader(Paths.get(filePath));) {
-			Iterator<IndianStateCode> iterator = getCSVFileIterator(reader, IndianStateCode.class, filePath);
+			Iterator<IndianStateCode> iterator = new OpenCSVBuilder().getCSVFileIterator(reader, IndianStateCode.class, filePath);
 			return getCount(iterator);
 		} catch (IOException e) {
 			throw new AnalyserException(AnalyserExceptionType.FILE_PROBLEM, "File problem encountered");
 		}
 	}
 
-	private <E> Iterator<E> getCSVFileIterator(Reader reader, Class<E> csvClass, String filePath)
-			throws AnalyserException {
-		try {
-			if (!((filePath.split("\\.")[1]).equals("csv"))) {
-				throw new AnalyserException(AnalyserExceptionType.INCORRECT_TYPE, "Incorrect file type");
-			}
-			if (!isCorrectDelimiter(filePath)) {
-				throw new AnalyserException(AnalyserExceptionType.INCORRECT_DELIMITER, "File contains Invalid Delimiter");
-			}
-			if (!isCorrectHeader(filePath)) {
-				throw new AnalyserException(AnalyserExceptionType.INCORRECT_HEADER, "Incorrect Header");
-			}
-			CsvToBean<E> csvToBean = new CsvToBeanBuilder<E>(reader).withType(csvClass)
-					                                                .withIgnoreLeadingWhiteSpace(true)
-					                                                .build();
-			return csvToBean.iterator();
-		} catch (IllegalStateException e) {
-			throw new AnalyserException(AnalyserExceptionType.UNABLE_TO_PARSE, "Unable to parse");
-		}
-	}
 
-	public boolean isCorrectHeader(String filePath) throws AnalyserException {
-		try (BufferedReader bufferedReader = Files.newBufferedReader(Paths.get(filePath));) {
-			String[] headerColumns = bufferedReader.readLine().split(",");
-			if (!headerColumns[1].equals("State Name") || !headerColumns[2].equals("TIN")
-					|| !headerColumns[3].equals("Population") || !headerColumns[4].equals("State Code"))
-				return false;
-		} catch (IOException e) {
-			throw new AnalyserException(AnalyserExceptionType.FILE_PROBLEM, "File Problem Occured");
-		}
-		return true;
-	}
-
-	public boolean isCorrectDelimiter(String filePath) throws AnalyserException {
-		try (BufferedReader bufferedReader = Files.newBufferedReader(Paths.get(filePath));) {
-			String[] headerColumns = bufferedReader.readLine().split(",");
-			if (headerColumns.length < 5) {
-				return false;
-			}
-		} catch (IOException e) {
-			throw new AnalyserException(AnalyserExceptionType.FILE_PROBLEM, "File Problem Occured");
-		}
-		return true;
-	}
 	private <E> int getCount(Iterator<E> iterator) {
 		Iterable<E> csvIterable = () -> iterator;
 		return (int) StreamSupport.stream(csvIterable.spliterator(), false).count();
