1a66e74c12a3984f8f0da052e3456c66eaa7e8e0
harshit031196 <guptaharshit0311@gmail.com>
2020-10-19 23:25:22 +0530

    added the exception file and made improvements

--- src/main/java/com/indianstatescensusanalyser/AnalyserException.java
+++ src/main/java/com/indianstatescensusanalyser/AnalyserException.java
@@ -7,13 +7,10 @@ public class AnalyserException extends Exception {
 	 */
 	private static final long serialVersionUID = 2495158559928012312L;
 	public enum AnalyserExceptionType{
-		FILE_NOT_FOUND_TYPE, INCORRECT_TYPE, DELIMITER_OR_HEADER_TYPE, OTHER_TYPE
+		FILE_PROBLEM, UNABLE_TO_PARSE, INCORRECT_TYPE, INCORRECT_DELIMITER, INCORRECT_HEADER;
 	}
 	private String message;
 	public AnalyserExceptionType type;
-	public AnalyserException() {
-		
-	}
 	public AnalyserException(AnalyserExceptionType type,String message){
 		super(message);
 		this.type=type;
--- src/main/java/com/indianstatescensusanalyser/CSVStateCensus.java
+++ src/main/java/com/indianstatescensusanalyser/CSVStateCensus.java
@@ -1,60 +1,44 @@
 package com.indianstatescensusanalyser;
 
 import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
-import java.net.URI;
 import java.nio.file.Files;
-import java.nio.file.NoSuchFileException;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.List;
+import java.util.stream.StreamSupport;
 
-import com.indianstatescensusanalyser.AnalyserException.AnalyserExceptionType;
-import com.opencsv.CSVReader;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
 
+import com.indianstatescensusanalyser.AnalyserException.AnalyserExceptionType;
+
 public class CSVStateCensus {
 
-	private static final String STRING_READ_SAMPLE = "IndiaStateCensusData - IndiaStateCensusData.csv";
-	private static final String STRING_READ_SAMPLE_NEW = "IndiaStateCode - IndiaStateCode.csv";
-	public enum IOStateCensusAnalyser {
-		CONSOLE_IO, FILE_IO, DB_IO, REST_IO
-	}
-	public int readFromCSVFile(IOStateCensusAnalyser ioService) throws IOException, AnalyserException{
-		if(ioService.equals(IOStateCensusAnalyser.FILE_IO)) {
-			int count = this.readToCSV();
-			return count;
-		}
-		return 0;
-	}
-
-	public int readToCSV() throws IOException, AnalyserException{
-		List<IndianStateCode> data = new ArrayList<IndianStateCode>();
-		int count=0;
-		try(Reader reader = Files.newBufferedReader(Paths.get(STRING_READ_SAMPLE_NEW))){
-			CsvToBeanBuilder<IndianStateCode> csvToBeanBuilder = new CsvToBeanBuilder<IndianStateCode>(reader);
-			csvToBeanBuilder.withType(IndianStateCode.class);
-			csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
-			CsvToBean<IndianStateCode> csvToBean = csvToBeanBuilder.build();
-			Iterator<IndianStateCode> objectIterator = csvToBean.iterator();
-			CSVReader csvReader = new CSVReader(reader);
-			while (objectIterator.hasNext()) {	
-				if(objectIterator.next()!=null) 
-					count++;
+	public int loadStateCensusData(String filePath) throws AnalyserException {
+		try (Reader reader = Files.newBufferedReader(Paths.get(filePath));) {
+			if (!(filePath.split("\\.")[1].equals("csv"))) {
+				throw new AnalyserException(AnalyserExceptionType.INCORRECT_TYPE, "Incorrect File Type");
 			}
-			return count;
-		}catch(IllegalStateException e){
-			throw new AnalyserException(AnalyserExceptionType.INCORRECT_TYPE, e.getMessage());
-		}
-		catch(NoSuchFileException e) {
-			throw new AnalyserException(AnalyserExceptionType.FILE_NOT_FOUND_TYPE,"No file found");
-		}
-		catch(RuntimeException e) {
-			throw new AnalyserException(AnalyserExceptionType.DELIMITER_OR_HEADER_TYPE,e.getMessage());
+			BufferedReader bufferedReader = Files.newBufferedReader(Paths.get(filePath));
+			String[] headerColumns = bufferedReader.readLine().split(",");
+			if (headerColumns.length < 5) {
+				throw new AnalyserException(AnalyserExceptionType.INCORRECT_DELIMITER, "File contains Invalid Delimiter");
+			}
+			if (!headerColumns[1].equals("State Name") || !headerColumns[2].equals("TIN")
+					|| !headerColumns[3].equals("Population") || !headerColumns[4].equals("State Code")) {
+				throw new AnalyserException(AnalyserExceptionType.INCORRECT_HEADER, "Incorrect Header");
+			}
+			bufferedReader.close();
+			CsvToBean<IndiaStateCensusData> csvToBean = new CsvToBeanBuilder<IndiaStateCensusData>(reader)
+					                                          .withType(IndiaStateCensusData.class).withIgnoreLeadingWhiteSpace(true).build();
+			Iterator<IndiaStateCensusData> iterator = csvToBean.iterator();
+			Iterable<IndiaStateCensusData> csvIterable = () -> iterator;
+			return (int) StreamSupport.stream(csvIterable.spliterator(), false).count();
+		} catch (IllegalStateException e) {
+			throw new AnalyserException(AnalyserExceptionType.UNABLE_TO_PARSE, "Unable to parse");
+		} catch (IOException e) {
+			throw new AnalyserException(AnalyserExceptionType.FILE_PROBLEM, "File problem encountered");
 		}
 	}
 }
